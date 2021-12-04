033f000faf28c299028710fdc4bb0db291e2b3bf
Kairui Cheng <kairuicheng@Zhihongs-iMac.local>
2021-12-04 10:04:14 -0800

    Trying to fix x^x and such

--- script.js
+++ script.js
@@ -3,24 +3,29 @@
  *
  * This calculator can calculate derivatives of single variable expressions.
  * This calculator can also calculate linear and quadratic approximations of functions.
+ * This calculator can also calculate roots of functions using Newton's method.
  */
-
 //---------------------------------------
 //This object defines the type of the expression.
-
 const EXPR_TYPE = {
   Number: "number",
   Variable: "variable",
   Sum: "sum",
+  Minus: "minus",
   Product: "product",
+  Division: "division",
   Power: "power",
-  Exponential: "exponential"
+  Exponential: "exponential",
+  Log: "log",
+  Sin: "sin",
+  Cos: "cos",
+  Tan: "tan",
+  Arcsin: "arcsin",
+  Arccos: "arccos",
+  Arctan: "arctan"
 };
-
-//---------------------------------------
 //---------------------------------------
 //This section is all about checking whether the type of an expression is a certain type.
-
 function is_Expr_Certain_Type(expr, type) {
   return expr.type === type;
 }
@@ -37,8 +42,16 @@ function is_sum(expr) {
   return is_Expr_Certain_Type(expr, EXPR_TYPE.Sum);
 }
 
+function is_minus(expr) {
+  return is_Expr_Certain_Type(expr, EXPR_TYPE.Minus)
+}
+
 function is_product(expr) {
   return is_Expr_Certain_Type(expr, EXPR_TYPE.Product);
+}
+
+function is_division(expr) {
+  return is_Expr_Certain_Type(expr, EXPR_TYPE.Division)
 }
 
 function is_power(expr) {
@@ -49,7 +62,10 @@ function is_exponential(expr) {
   return is_Expr_Certain_Type(expr, EXPR_TYPE.Exponential);
 }
 
-//---------------------------------------
+function is_log(expr) {
+  return is_Expr_Certain_Type(expr, EXPR_TYPE.Log);
+}
+
 //---------------------------------------
 //Derive is the central highway that leads to different formulas for
 //different derivatives for different expressions.
@@ -64,8 +80,14 @@ function derive(expr, variable) {
   if (is_sum(expr)) {
     return derive_sum(expr, variable);
   }
+  if (is_minus(expr)) {
+    return derive_minus(expr, variable);
+  }
   if (is_product(expr)) {
     return derive_product(expr, variable);
+  }
+  if (is_division(expr)) {
+    return derive_division(expr, variable);
   }
   if (is_power(expr)) {
     return derive_power(expr, variable);
@@ -73,10 +95,12 @@ function derive(expr, variable) {
   if (is_exponential(expr)) {
     return derive_exponential(expr, variable);
   }
+  if (is_log(expr)) {
+    return derive_log(expr, variable);
+  }
   return Error;
 }
 
-//---------------------------------------
 //---------------------------------------
 //Display_expr displays the nested objects as a mathematical expression.
 
@@ -90,8 +114,14 @@ function display_expr(expr) {
   if (is_sum(expr)) {
     return `(${display_expr(expr.operand1)} + ${display_expr(expr.operand2)})`;
   }
+  if (is_minus(expr)) {
+    return `(${display_expr(expr.operand1)} - ${display_expr(expr.operand2)})`;
+  }
   if (is_product(expr)) {
     return `(${display_expr(expr.operand1)} * ${display_expr(expr.operand2)})`;
+  }
+  if (is_division(expr)) {
+    return `(${display_expr(expr.operand1)} / ${display_expr(expr.operand2)})`;
   }
   if (is_power(expr)) {
     return `(${display_expr(expr.operand1)} ^ ${display_expr(expr.operand2)})`;
@@ -99,9 +129,12 @@ function display_expr(expr) {
   if (is_exponential(expr)) {
     return `(${display_expr(expr.operand1)} ^ ${display_expr(expr.operand2)})`;
   }
+  if (is_log(expr)) {
+    return `(ln(${display_expr(expr.operand1)}))`;
+  }
+
 }
 
-//---------------------------------------
 //---------------------------------------
 /*Basic_Simplify_Expr simplifies the mathematical expression (object) partially
 before it is converted into a readable expression. (incomplete)*/
@@ -113,7 +146,7 @@ function basic_simplify_expr(expr) {
   if (is_variable(expr)) {
     return expr;
   }
-  // expr is either a sum, or product, or power
+  // expr is either a sum/minus, or product/division, or power
   const part1 = basic_simplify_expr(expr.operand1);
   const part2 = basic_simplify_expr(expr.operand2);
   if (is_number(part1) && is_number(part2)) {
@@ -124,9 +157,17 @@ function basic_simplify_expr(expr) {
       result = p1v + p2v;
       return make_number(result);
     }
+    if (expr.type === EXPR_TYPE.Minus) {
+      result = p1v - p2v;
+      return make_number(result)
+    }
     if (expr.type === EXPR_TYPE.Product) {
       result = p1v * p2v;
       return make_number(result);
+    }
+    if (expr.type === EXPR_TYPE.Division) {
+      result = p1v / p2v
+      return make_number(result)
     }
     if (expr.type === EXPR_TYPE.Power || expr.type === EXPR_TYPE.Exponential) {
       result = p1v ^ p2v;
@@ -185,7 +226,6 @@ function basic_simplify_expr(expr) {
 }*/
 
 //---------------------------------------
-//---------------------------------------
 //These functions list the different types of directions for the
 //different types of expressions.
 
@@ -194,6 +234,10 @@ function derive_sum(expr, variable) {
     derive(expr.operand1, variable),
     derive(expr.operand2, variable)
   );
+}
+
+function derive_minus(expr, variable) {
+  return make_minus(derive(expr.operand1, variable), derive(expr.operand2, variable));
 }
 
 function derive_product(expr, variable) {
@@ -208,23 +252,69 @@ function derive_product(expr, variable) {
   return make_sum(part1, part2);
 }
 
-function derive_power(expr, variable) {
-  // expr = f ^ g (g is constant)
-  // expr' = g * f ^ (g - 1) * fp
+function derive_division(expr, variable) {
+  // expr = f / g
+  // expr' = (f' * g - g' * f) / (g ^ 2)
   const f = expr.operand1;
   const g = expr.operand2;
   const fp = derive(f, variable);
-  const part1 = make_product(g, fp);
-  const part2 = make_product(
-    part1,
-    make_power(f, make_sum(g, make_number(-1)))
-  );
-  return part2;
+  const gp = derive(g, variable);
+  const part1 = make_product(fp, g);
+  const part2 = make_product(gp, f);
+  const part3 = make_power(g, make_number(2))
+  return make_division(make_minus(part1, part2), part3);
 }
 
-function derive_exponential(expr, variable) {
+function derive_power(expr, variable) {
+  // expr = f ^ g (g is constant)
+  // expr' = g * f ^ (g - 1) * fp
+  // Needs general case
+  // If g is constant, then expr' = g * f ^ (g - 1) * fp
+  // If f is constant, then expr' = ln(f) * f ^ g * gp
+  // x ^ x = e ^ (ln(x) * x) => (ln(x) + 1) * x ^ x
+  // If both are not constant, then f ^ g = e ^ (ln(f) * g) => e ^ (ln(f) * g) * '(ln(f) * g)
+  const f = expr.operand1;
+  //console.log(f)
+  const g = expr.operand2;
+  //console.log(g)
+  const gp = derive(g, variable);
+  //console.log(gp)
+  const fp = derive(f, variable);
+  //console.log(fp)
+  if (is_number(g)) {
+    const part1 = make_product(g, fp);
+    const part2 = make_product(
+      part1,
+      make_power(f, make_sum(g, make_number(-1)))
+    );
+    return part2;
+  }
+  if (is_number(f) && is_number(g) !== true) {
+    const part1 = make_product(
+      make_number(special_functions("ln", parseFloat(display_expr(f)))),
+      expr
+    );
+    const part2 = make_product(part1, gp);
+    return part2;
+  }
+  if (is_number(f) !== true && is_number(g) !== true) {
+    console.log("HEEERE")
+    const part3 = make_product(make_log(f), g)
+    console.log(part3, "PART3")
+    const part2 = make_product(expr, derive(part3, "x"))
+    console.log(part2, "PART2")
+    return part2
+  }
+}
+
+function derive_log(expr, variable) {
+  const f = expr.operand1
+  return make_product(make_division(make_number(1), f), derive(f, variable))
+}
+
+/*function derive_exponential(expr, variable) {
   // expr = f ^ g (f is constant)
-  // expr' = ln(f) * (f ^ g)
+  // expr' = ln(f) * (f ^ g) * gp
   const f = expr.operand1;
   const g = expr.operand2;
   const gp = derive(g, variable);
@@ -234,9 +324,8 @@ function derive_exponential(expr, variable) {
   );
   const part2 = make_product(part1, gp);
   return part2;
-}
+}*/
 
-//---------------------------------------
 //---------------------------------------
 //Special_functions will soon be able to list all the special functions.
 
@@ -244,9 +333,17 @@ function special_functions(func, arg) {
   if (func === "ln") {
     return Math.log(arg) / Math.log(Math.E);
   }
+  if (func === "sin") {
+    return Math.sin(arg)
+  }
+  if (func === "cos") {
+    return Math.cos(arg)
+  }
+  if (func === "tan") {
+    return Math.tan(arg)
+  }
 }
 //console.log(special_functions("ln", 12345))
-//---------------------------------------
 //---------------------------------------
 //These next functions are the constructors for the expressions.
 
@@ -262,8 +359,16 @@ function make_product(operand1, operand2) {
   return make_expr(EXPR_TYPE.Product, operand1, operand2);
 }
 
+function make_division(operand1, operand2) {
+  return make_expr(EXPR_TYPE.Division, operand1, operand2);
+}
+
 function make_sum(operand1, operand2) {
   return make_expr(EXPR_TYPE.Sum, operand1, operand2);
+}
+
+function make_minus(operand1, operand2) {
+  return make_expr(EXPR_TYPE.Minus, operand1, operand2);
 }
 
 function make_power(operand1, operand2) {
@@ -272,6 +377,10 @@ function make_power(operand1, operand2) {
 
 function make_exponential(operand1, operand2) {
   return make_expr(EXPR_TYPE.Exponential, operand1, operand2);
+}
+
+function make_log(operand1) {
+  return make_expr(EXPR_TYPE.Log, operand1);
 }
 
 function make_expr(type, operand1, operand2) {
@@ -283,44 +392,26 @@ function make_expr(type, operand1, operand2) {
 }
 
 //---------------------------------------
-//---------------------------------------
-//These objects define the types of expressions.
+//These objects define the types of expressions. 
 
-const num_expr = {
-  type: EXPR_TYPE.Number,
-  operand1: 0
-};
+const num_expr = { type: EXPR_TYPE.Number, operand1: 0 };
 
-const variable_expr = {
-  type: EXPR_TYPE.Variable,
-  operand1: "x"
-};
+const variable_expr = { type: EXPR_TYPE.Variable, operand1: "x" };
 
-const sum_expr = {
-  type: EXPR_TYPE.Sum,
-  operand1: null,
-  operand2: null
-};
+const sum_expr = { type: EXPR_TYPE.Sum, operand1: null, operand2: null };
 
-const power_expr = {
-  type: EXPR_TYPE.Power,
-  operand1: null,
-  operand2: null
-};
+const minus_expr = { type: EXPR_TYPE.Sum, operand1: null, operand2: null };
 
-const product_expr = {
-  type: EXPR_TYPE.Product,
-  operand1: null,
-  operand2: null
-};
+const power_expr = { type: EXPR_TYPE.Power, operand1: null, operand2: null };
 
-const exponential_expr = {
-  type: EXPR_TYPE.Exponential,
-  operand1: null,
-  operand2: null
-};
+const product_expr = { type: EXPR_TYPE.Product, operand1: null, operand2: null };
 
-//---------------------------------------
+const division_expr = { type: EXPR_TYPE.Division, operand1: null, operand2: null };
+
+const exponential_expr = { type: EXPR_TYPE.Exponential, operand1: null, operand2: null };
+
+const log_expr = { type: EXPR_TYPE.Log, operand1: null, operand2: null };
+
 //---------------------------------------
 //These next few functions help solve for the roots of an equation.
 
@@ -328,19 +419,8 @@ function average(x, y) {
   return (x + y) / 2;
 }
 
-function positive(x) {
-  return x > 0;
-}
-function negative(x) {
-  return x < 0;
-}
-
-function abs(x) {
-  return x >= 0 ? x : -x;
-}
-
 function close_enough(x, y) {
-  return abs(x - y) < 0.0000001;
+  return Math.abs(x - y) < 0.0000001;
 }
 
 function search(f, neg_point, pos_point) {
@@ -349,9 +429,9 @@ function search(f, neg_point, pos_point) {
     return midpoint;
   } else {
     const test_value = f(midpoint);
-    if (positive(test_value)) {
+    if (Math.sign(test_value) > 0) {
       return search(f, neg_point, midpoint);
-    } else if (negative(test_value)) {
+    } else if (Math.sign(test_value) < 0) {
       return search(f, midpoint, pos_point);
     } else {
       return midpoint;
@@ -362,9 +442,9 @@ function search(f, neg_point, pos_point) {
 function half_interval_method(f, a, b) {
   const a_value = f(a);
   const b_value = f(b);
-  return negative(a_value) && positive(b_value)
+  return Math.sign(a_value) < 0 && Math.sign(b_value)
     ? search(f, a, b)
-    : negative(b_value) && positive(a_value)
+    : Math.sign(b_value) < 0 && Math.sign(a_value) > 0
       ? search(f, b, a)
       : error("values are not of opposite sign");
 }
@@ -382,7 +462,6 @@ function fixed_point(f, first_guess) {
   return try_with(first_guess, 1);
 }
 
-//---------------------------------------
 //---------------------------------------
 //This part parses the string into objects. str -> object which goes through the calculator functions
 
@@ -428,7 +507,7 @@ const infixToPostfix = (infix) => {
   infix = infix.split(/([\+\-\*\/\^\(\)])/).clean();
   for (var i = 0; i < infix.length; i++) {
     var token = infix[i];
-    if (token.isNumeric()) {
+    if (token.isNumeric() || /^[A-Za-z]+$/.test(token)) {
       outputQueue += token + " ";
     } else if ("^*/+-".indexOf(token) !== -1) {
       var o1 = token;
@@ -438,8 +517,6 @@ const infixToPostfix = (infix) => {
         o2 = operatorStack[operatorStack.length - 1];
       }
       operatorStack.push(o1);
-    } else if (/^[A-Za-z]+$/.test(token)) {
-      operatorStack.push(token);
     } else if (token === "(") {
       operatorStack.push(token);
     } else if (token === ")") {
@@ -460,23 +537,25 @@ const solvePostfix = (postfix) => {
   postfix = postfix.split(" ");
   for (var i = 0; i < postfix.length; i++) {
     if (postfix[i].isNumeric()) {
-      resultStack.push(make_number(postfix[i]));
+      resultStack.push(make_number(parseFloat(postfix[i])));
     } else {
       if (postfix[i] === "x") {
         resultStack.push(make_variable(postfix[i]))
       } else {
         var a = resultStack.pop();
+        //console.log(a, "aaaaaaaaaaaaa")
         var b = resultStack.pop();
+        //console.log(b, "bbbbbbbbbbbb")
         if (postfix[i] === "+") {
-          resultStack.push(make_sum(a, b));
+          resultStack.push(make_sum(b, a));
         } else if (postfix[i] === "-") {
-          //resultStack.push(parseInt(b) - parseInt(a));
+          resultStack.push(make_minus(b, a));
         } else if (postfix[i] === "*") {
-          resultStack.push(make_product(a, b));
+          resultStack.push(make_product(b, a));
         } else if (postfix[i] === "/") {
-          //resultStack.push(parseInt(b) / parseInt(a));
+          resultStack.push(make_division(b, a));
         } else if (postfix[i] === "^") {
-          resultStack.push(make_power(a, b));
+          resultStack.push(make_power(b, a));
         }
       }
     }
@@ -484,20 +563,50 @@ const solvePostfix = (postfix) => {
   if (resultStack.length > 1) {
     return "error";
   } else {
-    console.log(resultStack)
+    //console.log(resultStack) 
     return resultStack.pop();
   }
 }
 
-function tester(object) {
-  return display_expr(basic_simplify_expr(derive(object, "x")))
+function initialize() {
+  resetText()
 }
-console.log(infixToPostfix("2+3+4"))
-let expr = solvePostfix(infixToPostfix("6*x"))
-//console.log(expr)
-console.log(tester(expr))
-//console.log(display_expr(basic_simplify_expr(derive(solvePostfix(infixToPostfix("6*x")), "x"))))
 
+function calculate() {
+  initialize()
+  const element = document.getElementById("strtext")
+  console.log(element)
+  let result = parser_derive(element.value)
+  console.log(result)
+  render(result)
+}
+
+function render(text) {
+  let newText = document.getElementById("output")
+  newText.innerHTML = text
+  console.log(text)
+}
+
+function resetText() {
+  let newText = document.getElementById("output")
+  newText.innerHTML = ""
+}
+
+function parser_derive(str) {
+  return display_expr(basic_simplify_expr(derive(solvePostfix(infixToPostfix(str)), "x")))
+}
+
+function parser(str) {
+  return solvePostfix(infixToPostfix(str));
+}
+//console.log(infixToPostfix("6*x^2"))
+//let expr = solvePostfix(infixToPostfix("3*x^3"))
+//console.log(expr)
+//console.log(display_expr(basic_simplify_expr(parser("3"))))
+//console.log(parser_derive("2 * 2 * 2 * x - 4 * x"))
+//console.log(display_expr(basic_simplify_expr(derive(solvePostfix(infixToPostfix("6*x")), "x"))))
+//console.log(make_product(make_power(make_variable("x"), make_number(2)), make_number(6)))
+//console.log(tester(make_product(make_power(make_variable("x"), make_number(2)), make_number(6)))) 
 //---------------------------------------
 //---------------------------------------
 //console.log(parse("(1 + 2) / 3)"))
@@ -513,9 +622,13 @@ const pow1 = make_power(x, make_number(2));
 
 const expr1 = make_sum(pow1, number1);
 const fexpr = make_expr(EXPR_TYPE.Power, expr1, number1);
-//console.log(display_expr(basic_simplify_expr(derive(fesxpr, "x"))));
+//console.log(display_expr(basic_simplify_expr(derive(fexpr, "x"))));
 //console.log(display_expr(derive(make_expr(EXPR_TYPE.Exponential, number1, expr1), "x")))
 //expr2 = (4*x)^4
 //console.log(expr1)
 //console.log(display_expr(expr1))
 //console.log(display_expr(basic_simplify_expr(derive(expr2, "x"))))
+//console.log(display_expr(derive(make_log(x), "x")))
+console.log(display_expr(derive(make_product(make_log(x), x), "x")))
+console.log(parser_derive("2^x"))
+console.log(parser_derive("x^x"))
